52d394fe7c2bae357cea4836518ffd566fa0e624
vishalibitwar <vish31ibitwar@gmail.com>
2020-03-25 11:11:46 +0530

    Updates

---
+++ precache-manifest.bbb6f6ef504f360da73a8cda0fe4c05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd95f0b3d2dc7e1ad346528804ff3cf",
+    "url": "/TasksList/index.html"
+  },
+  {
+    "revision": "2d64712d83753f777550",
+    "url": "/TasksList/static/css/main.52fae6db.chunk.css"
+  },
+  {
+    "revision": "7e5c8829f4ea5eeb59bf",
+    "url": "/TasksList/static/js/2.bb173171.chunk.js"
+  },
+  {
+    "revision": "2d64712d83753f777550",
+    "url": "/TasksList/static/js/main.cc299264.chunk.js"
+  },
+  {
+    "revision": "222d290645eb47b5cf21",
+    "url": "/TasksList/static/js/runtime-main.f2223dc3.js"
+  },
+  {
+    "revision": "5b05acfab2ab9629626c697005c4b82e",
+    "url": "/TasksList/static/media/background.5b05acfa.png"
+  }
+]);
